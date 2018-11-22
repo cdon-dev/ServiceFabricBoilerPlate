0b66bc5e271f113403e6b5665fa239c937335c56
Per Ökvist <perokvist@users.noreply.github.com>
2018-11-22 11:13:40 +0100

    EventHubs CommunicationListener

---
+++ src/CommunicationListener.EventHubs/EventHubFabricReceiverConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunicationListener.EventHubs
+{
+	public class EventHubFabricReceiverConfiguration
+	{
+		public string ConnectionString { get; internal set; }
+		public string OffsetDictionaryName { get; internal set; }
+		public string EpochDictionaryName { get; internal set; }
+		public string ConsumerGroup { get; internal set; }
+	}
+}
